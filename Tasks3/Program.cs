e7bd121b510fd10dce36c5d935426a0dc2e04786
KrayniyDA <Krayniy19730507@gmail.com>
2022-05-15 04:53:11 +0700

    first commit

---
+++ Tasks3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. ");
+Console.Write("Введите трёхзначное число: ");
+ string a = Convert.ToString(Console.ReadLine());
+ThirdDigit(a);
+
+void ThirdDigit(string number)
+{
+    if (number.Length >= 3)
+    {
+        Console.WriteLine(number[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет! ");
+    }
+}
+
