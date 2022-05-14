1daf4e9ccc1f2b339c6a249a98c43230c6a45c8e
KrayniyDA <Krayniy19730507@gmail.com>
2022-05-15 04:52:49 +0700

    first commit

---
+++ Tasks1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. ");
+Console.Write("Введите трехзначное число:");
+string s = Console.ReadLine();
+    while (s.Length !=3)
+{
+    Console.Write("Вы ввели не трехзначное число, введите заного трехзначное число: ");
+    s = Console.ReadLine();
+}
+    Console.WriteLine(s[1]);
+
+    
